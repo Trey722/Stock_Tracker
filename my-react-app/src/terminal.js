efd48c874f66238863e1adcf06917e3c81cb7685
RougeAce <127271451+RougeAce@users.noreply.github.com>
2024-01-06 13:32:54 -0600

    Organzised files

--- my-react-app/src/terminal.js
+++ my-react-app/src/terminal.js
@@ -1,100 +1,19 @@
 import React, { useState, useRef } from 'react';
-import StockPriceComponent from './getStockInfo';
-import OptionsChainComponent from './getOptionInfo';
-import RiskFreeComponent from './getRiskFreeRate';
-import OptionsProfitAtEachPrice from './calcOptions';
-import getGreeks from './getOptionGreeks';
-import StockGreeksComponent from './getOptionGreeks';
-
-
-function parseCommand(string)
-{
-    let arrayString = string.split(' ')
-    console.log(arrayString);
-    return arrayString
-}
-
-function excuteGetOption(command)
-{
-  if (command[2] === 'CHAIN')
-  {
-    if (command[3] && command[4])
-    {
-      return <OptionsChainComponent stock={command[3]} expiration_date={command[4]}/>
-    }
-  }
-
-  else if (command[2] === 'GREEKS')
-  {
-    console.log(command[3])
-    console.log("Attempting to get option greeks for", command[3])
-    return <StockGreeksComponent stock={command[3]}/>
-  }
-
-  else {
-    return failedToExcute("Missing date")
-  }
-}
-
-
-
-function excuteGetRequest(command)
-{
-  console.log(command[1]);
-  if (command[1] == 'STOCK')
-  {
-    if (command[2])
-    {
-      console.log("Tried to get STOCK for", command[2]); 
-    
-      return <StockPriceComponent stock={command[2]}/>
-    }
-
-    return failedToExcute("Missing stock")
-  }
-
-  else if (command[1] == 'OPTION')
-  {
-    return excuteGetOption(command)
-  }
-
-  else if (command[1] == "ECON")
-  {
-    if (command[2] == 'risk_free_rate') return <RiskFreeComponent/>
-    else 
-    {
-      return failedToRead(command)
-    }
-  }
-
-  else {return failedToRead(command)}
-}
+import StockPriceComponent from './COMP/stockComp/getStockInfo';
+import OptionsChainComponent from './input/get/optionGet/components/getOptionInfo';
+import RiskFreeComponent from './input/get/econGet/components/getRiskFreeRate';
+import OptionsProfitAtEachPrice from './COMP/optionComp/calcOptions';
+import getGreeks from './input/get/optionGet/components/getOptionGreeks';
+import StockGreeksComponent from './input/get/optionGet/components/getOptionGreeks';
+import procceseCommands from "./input/paraseCommands"
 
 
 
 
-function failedToRead(command)
-{
-    return (
-        <div>
-          <p className="error">Could not find command '{command}'.</p>
-          <p>Type -help for help.</p>
-          
-        </div>
-      );
 
-}
 
-function failedToExcute(error)
-{
-  return (
-  <div>
-          <p className="error"> Failed Excution: {error}</p>
-          <p>Type -help for help.</p>
-          
-        </div>
-    )
-}
+
+
 
 
 
@@ -117,30 +36,12 @@ const Terminal = () => {
 
   const processInput = (command) => {
     let outputContent;
-    let parsaedCommand
-
-  
-    parsaedCommand = parseCommand(command);
+    
+    outputContent = procceseCommands(command)
 
 
     
-    if (parsaedCommand[0] == "--get")
-    {
-      console.log("Excuting get request")
-      console.log("We received command", command);
-      outputContent = excuteGetRequest(parsaedCommand);
-      
-    }
-
-    else if (parsaedCommand == "--help")
-    {
-      //return help stuff
-    }
-
-    else {
-        console.log("Emergncy Else excuted")
-      outputContent = failedToRead(command)
-    }
+    
 
     // Set the output, including the entered command
     setOutput((prevOutput) => [
